c9417fc0ae919be28bcae8b1c98faad6598ee410
alexfoures <fouresalexandre@gmail/com>
2020-12-03 22:34:46 +0100

    recherche front

--- NUitDeLInfo2020/view/plage/index.php
+++ NUitDeLInfo2020/view/plage/index.php
@@ -1,18 +1,21 @@
 <link rel="stylesheet" href="css/plage.css">
-<div>
-<div class="empty"></div>
-<div class="search-content row">
-    <div class="column half-width">
-        <div class="text text-1">Trouvez</div>
-        <div class="text text-2">votre plage de rêve</div>
-        <div class="row inline all-width">
-          <button class="mybtn" type="submit"><i class="fa color-white fa-search"></i></button>
-          <input class="myinput" type="text" placeholder="Votre plage" aria-label="Search">
+
+<div class="container-fluid">
+    <div class="row search-content">
+        <div class="col-lg-6">
+            <div class="text text-1">Trouvez</div>
+            <div class="text text-2">votre plage de rêve</div>
+            <div class="row inline all-width">
+            <button class="mybtn" type="submit"><i class="fa color-white fa-search"></i></button>
+            <input class="myinput" type="text" placeholder="Votre plage" aria-label="Search">
+            </div>
+        </div>
+
+        <div class="col-md-offset-3 col-lg-3">
+            <img src="https://cybersavoir.csdm.qc.ca/bibliotheques/files/2018/11/10_banques_dimages_gratuites_libres_de_droits-300x169.jpg" alt="">
         </div>
     </div>
-    <div class="text-1">element</div>
 </div>
-
 
 <?php
 foreach ($d as $key => $value){

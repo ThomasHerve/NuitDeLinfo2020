3717c67ce83878a78274be2444b7fc7b2bf11526
alexfoures <fouresalexandre@gmail/com>
2020-12-04 01:09:58 +0100

    pagination

--- NUitDeLInfo2020/Controller/PlageController.php
+++ NUitDeLInfo2020/Controller/PlageController.php
@@ -6,14 +6,65 @@ class PlageController extends Controller
 	{
 	}
 
+	public function getPlage() {
+
+			// On détermine sur quelle page on se trouve
+		if(isset($_GET['page']) && !empty($_GET['page'])){
+			$currentPage = (int) strip_tags($_GET['page']);
+		}else{
+			$currentPage = 1;
+		}
+
+		$array = [
+			["note" => 5,"nom" => "1","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 4.5,"nom" => "2","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 2.3,"nom" => "3","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 5,"nom" => "4","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 4.5,"nom" => "5","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 2.3,"nom" => "6","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 5,"nom" => "7","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 4.5,"nom" => "8","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 2.3,"nom" => "9","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 5,"nom" => "10","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 4.5,"nom" => "11","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 2.3,"nom" => "12","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 5,"nom" => "13","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 4.5,"nom" => "14","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 2.3,"nom" => "15","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 5,"nom" => "16","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 4.5,"nom" => "17","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
+			["note" => 2.3,"nom" => "18","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"]
+		];
+
+		//requete sql recupérer le nombre de plage 
+		$nbPlages = sizeof($array);
+
+		//nb plages par pages
+		$parPage = 3;
+
+		//nombre de page
+		$pages = ceil($nbPlages/$parPage);
+
+		//determine le premier élément
+		$premier = ($currentPage * $parPage) - $parPage;
+
+		$array = array_slice($array, $premier, 3);
+
+		return $array;
+
+	}
+
 	public function index()
 	{
+		$array = $this->getPlage();
 		//note nom photo
+		/*
 		$array = [
 			["note" => 5,"nom" => "ching chong","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
 			["note" => 4.5,"nom" => "ching chong","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"],
 			["note" => 2.3,"nom" => "ching chong","photo" =>"https://viago.ca/wp-content/uploads/2015/07/Plage-768x432.jpg"]
 		];
+		*/
 		$this->render("index",$array);
 	}
 
--- NUitDeLInfo2020/view/plage/index.php
+++ NUitDeLInfo2020/view/plage/index.php
@@ -30,6 +30,27 @@ foreach ($d as $key => $value){
             <div class="Stars" style="--star-size: 35px;--rating: <?php echo $value["note"]; ?>;">★★★★★</div>
         </div>
     </div>
+
+    <nav>
+        <ul class="pagination">
+            <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
+            <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                <a href="./?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
+            </li>
+            <?php for($page = 1; $page <= $nbPlages; $page++): ?>
+                <!-- Lien vers chacune des nbPlages (activé si on se trouve sur la page correspondante) -->
+                <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                    <a href="./?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+                </li>
+            <?php endfor ?>
+                <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
+                <li class="page-item <?= ($currentPage == $nbPlages) ? "disabled" : "" ?>">
+                <a href="./?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
+            </li>
+        </ul>
+    </nav>
+
+    
 <?php
 }
 ?>

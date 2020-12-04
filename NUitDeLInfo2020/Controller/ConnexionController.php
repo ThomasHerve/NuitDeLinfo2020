8c3341fd4c984979f2caeb7025ea143d1ff9b6bc
MiniGabert <etienne.gabert@gmail.com>
2020-12-04 02:52:10 +0100

    findev -> test

--- NUitDeLInfo2020/Controller/ConnexionController.php
+++ NUitDeLInfo2020/Controller/ConnexionController.php
@@ -12,14 +12,18 @@ class ConnexionController extends Controller
     public function index()
     {
         if(isset($_POST['connexion'])) { 
-            $this->form($this);
+            $this->formco();
+
+        } elseif(isset($_POST['creeruser'])){
+            $this->formins();
+
         } else {
             $this->render("index");
         }
         
     }
 
-    public function form()
+    public function formco()
     {   
 
         if(empty($_POST['identifiant'])) {
@@ -37,31 +41,61 @@ class ConnexionController extends Controller
                 $ident = htmlentities($_POST['identifiant'], ENT_QUOTES, "ISO-8859-1"); 
                 $mdp   = htmlentities($_POST['mdp'], ENT_QUOTES, "ISO-8859-1");
                 
-                header("Location: ?r=site");
-
-                // ======= CODE CI-DESSOUS A TESTER UNE FOIS LA CLASSE DTLUsers TERMINÉ ========
-
-                //on se connecte à la base de données:
-                /*
                 $dtlusers = new DTLUsers();
                 $result =  $dtlusers->getByPseudo($ident, $mdp);
 
                 // Verification retour requete user
-                if( len($Requete) == 0) {
+                if( $result == NULL) {
                     $array = ["erreur" => "Le pseudo ou le mot de passe est incorrect, le compte n'a pas été trouvé"];
                     $this->render("index", $array);
 
                 } else {
                     // on ouvre la session avec $_SESSION:
-                    $_SESSION['id'] = result[0]->id(); // la session peut être appelée différemment et son contenu aussi peut être autre chose que le pseudo
+                    $_SESSION['id'] = $result->id; // la session peut être appelée différemment et son contenu aussi peut être autre chose que le pseudo
                     
                     // retour a l'acceuil
+                    header("Location: ?r=site");
 
                 }
-                */
             }
         }
-        
+    }
+
+    public function formins()
+    {   
+
+        if(empty($_POST['identifiantins'])) {
+            $array = ["erreurins" => "L'identifiant est vide"];;
+            $this->render("index", $array);
+
+        } else {
+            if(empty($_POST['mdpins'])) {
+                $array = ["erreurins" => "Le mot de passe est vide"];
+                $this->render("index", $array);
+
+            } else {
+
+                // le htmlentities() passera les guillemets en entités HTML, ce qui empêchera les injections SQL
+                $ident = htmlentities($_POST['identifiantins'], ENT_QUOTES, "ISO-8859-1"); 
+                $mdp   = htmlentities($_POST['mdpins'], ENT_QUOTES, "ISO-8859-1");
+                
+                $dtlusers = new DTLUsers();
+                $result =  $dtlusers->SetUsers($ident, $mdp);
+
+                // Verification retour requete user
+                if( $result == NULL) {
+                    $array = ["infoscreer" => "Cet utilisateur éxiste deja."];
+                    $this->render("index", $array);
+
+                } else {
+                    // on ouvre la session avec $_SESSION:
+                    
+                    $_SESSION['id'] = $result[0]; // la session peut être appelée différemment et son contenu aussi peut être autre chose que le pseudo
+                    
+                    $array = ["infoscreer" => "Utilisateur créé"];
+                    $this->render("index", $array);
+                }
+            }
+        }
     }
 }
-
